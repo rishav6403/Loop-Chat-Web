313a8e268cd944abb1a5928716e42ce5b466745a
Rishav Kumar <rishavkumar6403@gmail.com>
2024-09-23 13:23:22 +0530

    socket and sond update

--- Frontend/src/hooks/useListenMesaages.js
+++ Frontend/src/hooks/useListenMesaages.js
@@ -1,22 +1,21 @@
 import { useEffect } from "react";
 import { useSocketContext } from "../context/SocketContext";
 import useConversation from "../zustand/useCoversation";
-import notificationSound from '../assets/sounds/sound.mp3'
+import notificationSound from "../assets/sounds/notification.mp3";
 
 const useListenMesaages = () => {
+  const { socket } = useSocketContext();
+  const { messages, setMessages } = useConversation();
 
-   const{socket} = useSocketContext();
-   const{messages,setMessages} = useConversation();
-   
-useEffect(()=>{
-    socket?.on("newMessage",(newMessage)=>{
-        newMessage.shouldShake = true;
-        const sound = new Audio(notificationSound);
-        sound.play();
-        setMessages([...messages, newMessage])
-    } )
-    return ()=> socket?.off("newMessage")
-},[socket, setMessages, messages])
-}
+  useEffect(() => {
+    socket?.on("newMessage", (newMessage) => {
+      newMessage.shouldShake = true;
+      const sound = new Audio(notificationSound);
+      sound.play();
+      setMessages([...messages, newMessage]);
+    });
+    return () => socket?.off("newMessage");
+  }, [socket, setMessages, messages]);
+};
 
 export default useListenMesaages;
