49b8dde236d8fbf792f0a37977c3dfb84fc335d0
Rishav Kumar <rishavkumar6403@gmail.com>
2024-09-23 11:46:28 +0530

    end

--- Backend/server.js
+++ Backend/server.js
@@ -1,36 +1,32 @@
-import express from 'express';
+import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
 import cookieParser from "cookie-parser";
-import path from 'path'
+import path from "path";
 
 import connectToMongodb from "./db/connections.js";
 import authRoute from "./routes/authRoute.js";
 import messagesRoute from "./routes/messagesRoute.js";
-import userRoute from "./routes/userRoute.js"
-import { app, server } from './socket/socket.js';
-
+import userRoute from "./routes/userRoute.js";
+import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 8000;
 
-const __dirname = path.resolve()
-
-dotenv.config();
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "/Frontend/dist")))
+app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
 app.use("/api/auth", authRoute);
 app.use("/api/messages", messagesRoute);
-app.use("/api/users", userRoute)
+app.use("/api/users", userRoute);
 
-app.get("*", (req, res)=>{
-res.sendFile(path.join(__dirname, "Frontend", "dist","index.html"))
-})
-
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
-  connectToMongodb()
+  connectToMongodb();
   console.log(`Server started at ${PORT}`);
 });
